721f73d1401c4f539a744271a058492f0b47dccd
Rafael Caricio <rafael.jacinto@gmail.com>
2016-02-23 18:21:49 +0100

    Move through windows like my old VIm config

--- emacs.d/personal/emacs_config.el
+++ emacs.d/personal/emacs_config.el
@@ -20,3 +20,9 @@
 ;;enable tool and menu bars - good for beginners
 (tool-bar-mode 1)
 (menu-bar-mode 1)
+
+;; Personalized keybindings
+(define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
